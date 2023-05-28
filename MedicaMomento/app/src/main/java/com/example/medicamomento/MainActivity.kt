a3c34d294058f450a0e97876220fa0a901bedc32
unknown <zs20022115@estudiantes.uv.mx>
2023-05-28 13:42:40 -0600

    agregar medicamento

--- MedicaMomento/app/src/main/java/com/example/medicamomento/MainActivity.kt
+++ MedicaMomento/app/src/main/java/com/example/medicamomento/MainActivity.kt
@@ -1,26 +1,37 @@
 package com.example.medicamomento
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.BaseColumns
 import androidx.appcompat.widget.Toolbar
 import android.view.MenuItem
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //boton agregar medicamentos
+        val btnMasmedic : FloatingActionButton = findViewById(R.id.btnMedicamento)
+        btnMasmedic.imageTintList = ColorStateList.valueOf(Color.WHITE)
+        btnMasmedic.setOnClickListener {
+            startActivity(Intent(applicationContext, AgregarMedicamento::class.java))
+        }
         //bottomnav
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigationView.selectedItemId = R.id.bnInicio
